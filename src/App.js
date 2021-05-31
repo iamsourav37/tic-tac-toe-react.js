3bed841cf83f551ce67f96e3ae02b896cac6e6f6
iamsourav37 <souravganguly108@gmail.com>
2021-05-31 14:41:40 +0530

    declare the structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Icon from "./components/Icon";
 
+// react toastify
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// bootstrap
+import { Card, CardBody, Button, Container, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+const itemArray = new Array(9).fill("empty");
 function App() {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const checkWin = () => {
+    // checking the winner
+  };
+
+  const reload = () => {
+    // responsible for reloding the game
+  };
+
+  const changeItem = (itemNumber) => {
+    // change the icon in the ui
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>hello world</h1>
+      <Icon />
     </div>
   );
 }
---
+++ src/components/Icon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa";
+
+function Icon({ name }) {
+  switch (name) {
+    case "cross":
+      return <FaTimes className="icon" />;
+    case "circle":
+      return <FaRegCircle className="icon" />;
+
+    default:
+      return <FaPen className="icon" />;
+  }
+}
+
+export default Icon;
